f51368ab76ee7c7bd6fc59804293e6369351c080
gyaurib <tripcr6@gmail.com>
2020-05-31 21:56:55 -0500

    Subir Repos

---
+++ spring-boot-web/src/main/java/com/example/demo/controllers/IndexController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+	
+	@GetMapping({"/index", "/", "/home"})
+	public String index() {
+		
+		return "index";
+	}
+	
+	
+}
